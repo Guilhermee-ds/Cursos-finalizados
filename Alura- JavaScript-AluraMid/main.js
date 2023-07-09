507581043a691319781f9da00307a1514f1107fd
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-07-09 14:40:03 -0300

    creat while

--- Alura- JavaScript-AluraMid/main.js
+++ Alura- JavaScript-AluraMid/main.js
@@ -1,10 +1,24 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 
-    return;
 }
 
-//Acessar elementos do HTML no JavaScript
-document.querySelector('.tecla_pom').onclick = tocaSomPom; 
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log("contador ");
+}
+
+
+
 
 

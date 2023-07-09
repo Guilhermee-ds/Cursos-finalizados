876704f8b9606e87d13010f986c85b04460bfd9a
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-07-09 13:54:23 -0300

    creat function

---
+++ Alura- JavaScript-AluraMid/main.js
@@ -0,0 +1,10 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+
+    return;
+}
+
+//Acessar elementos do HTML no JavaScript
+document.querySelector('.tecla_pom').onclick = tocaSomPom; 
+
+

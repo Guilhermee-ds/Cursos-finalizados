f15ccf4f77a83f409653b65484aa34165bafd0db
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-10 21:38:08 -0300

    create method post in forms

--- laravelProject/app/Http/Controllers/ContatoController.php
+++ laravelProject/app/Http/Controllers/ContatoController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\VarDumper\VarDumper;
 
 class ContatoController extends Controller
 {
     public function contato() {
+
+        var_dump($_GET);
         return view('site.contato');
     }
 }

50e9288dc2f3bf48f14cb20c42a8e0923f874335
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-12 14:47:17 -0300

    create database

--- laravelProject/database/migrations/2023_08_11_184934_create_site_contatos_table.php
+++ laravelProject/database/migrations/2023_08_11_184934_create_site_contatos_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name',50);
+            $table->string('phone',10);
+            $table->string('email',80);
+            $table->integer('motive');
+            $table->text('message');
+
+
         });
     }
 

46bfc633524f76b4b456265ab9f991b1282e9bbe
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-15 21:05:11 -0300

    init: starting javaScript course

---
+++ leraningjs-fundamentals/arrays.js
@@ -0,0 +1,27 @@
+/*
+for create one array for armazaning list you can use this function
+ */
+
+//array list number
+var Myarray = [1,2,3,4,5,6,7,8,9,10]
+console.log(Myarray);
+
+
+//array list object
+
+var Myobject = {
+    "name": "car",
+    "price": `R\$1000`,
+    "responsible": "Guilherme",
+}
+
+console.log(Myobject);
+
+//look object is true or false
+
+var isTrue = true;
+var isFalse = false;
+
+function myFunction() {
+    console.log("Hello world");
+}

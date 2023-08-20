f3b21a1f91622b15d26fd8dfaf3f1c67ee6c6288
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-20 15:35:03 -0300

    learning Splice

--- leraningjs-fundamentals/manipulating-Arrays.js
+++ leraningjs-fundamentals/manipulating-Arrays.js
@@ -15,3 +15,13 @@ console.log(numbers); // exit [1,2,3,4,5,6,7,8,9] ADD element for final of array
 let numbersPOP = [1,2,3]
 numbersPOP.pop();
 console.log(numbersPOP); // exit [1,2] remove final element for array
+
+
+/**
+ * Splice
+ */
+
+
+let numbersSplice = [1,2,3,4];
+numbersSplice.splice(2,1); // remove element in position 1
+console.log(numbersSplice); // exit [1,2,4];

826724d378a844cfd2cc4aa7705dab8de270460e
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-31 16:04:32 -0300

    implementation condition and clean code

--- Alura-Comecando-com-a-linguagem/games/divination.py
+++ Alura-Comecando-com-a-linguagem/games/divination.py
@@ -12,9 +12,19 @@ my_number = (int(input("Enter your number: ")))
 #print my number
 print("You enter", my_number)
 
-if (number_secret == my_number):
+
+#condition
+right = my_number == number_secret
+larger = my_number > number_secret
+lower = my_number < number_secret
+
+if (right):
     print("you got the number right", number_secret)
-else: 
-    print("number incorrect")
+else:
+    if (larger):
+        print("your number is larger than the secret number")
+    elif (lower):
+        print("your number is lower than the secret number")
+        print("number incorrect")
 
 print("End-Game")

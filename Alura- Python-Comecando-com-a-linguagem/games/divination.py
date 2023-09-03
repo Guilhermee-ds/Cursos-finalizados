11df8cb250211a538b05d8b28f54ab31c77141e6
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-09-03 08:22:15 -0300

    method for

--- Alura- Python-Comecando-com-a-linguagem/games/divination.py
+++ Alura- Python-Comecando-com-a-linguagem/games/divination.py
@@ -6,16 +6,19 @@ print("-" * 100)
 # number secret
 number_secret = 42
 total_try = 3
-round = 1
 
 
-while round <= total_try:
+for round in range(1, total_try + 1):
     print("Try:{} of {}".format(round, total_try))  # string intapulation
     # colect my number
     my_number = int(input("Enter your number: "))
 
     # print my number
     print("You enter", my_number)
+
+    if my_number < 1 or my_number > 100:
+        print("Please enter number large 1")
+        continue
 
     # condition
     right = my_number == number_secret
@@ -24,13 +27,12 @@ while round <= total_try:
 
     if right:
         print("you got the number right", number_secret)
+        break
     else:
         if larger:
             print("your number is larger than the secret number")
         elif lower:
             print("your number is lower than the secret number")
             print("number incorrect")
-
-        round = round + 1
 
 print("End-Game")

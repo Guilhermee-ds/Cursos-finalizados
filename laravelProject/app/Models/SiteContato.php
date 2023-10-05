166e8ba6fd667f12a954225f9f08cdc8cd6798d5
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-05 10:51:00 -0300

    Eloquent - Adjusting the table name in the Model for a correct ORM

--- laravelProject/app/Models/SiteContato.php
+++ laravelProject/app/Models/SiteContato.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//status laravel save models in database
+//Site_Contato
+//site_contato
+//site_contatos => save this models in database
+
 class SiteContato extends Model
 {
     use HasFactory;
+
 }
